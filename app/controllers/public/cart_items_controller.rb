af9e6c68aadee4c07c2a34a5e4bcfde68a3d8b34
yuki-la-neige <noto.yuki.infratop@gmail.com>
2020-04-12 19:56:44 +0900

    modified cart_items#create

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,13 +7,21 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart = current_end_user.cart_items.new(cart_item_params)
-    if @cart.save
-      flash[:notice] = "Quantity was successfully created"
+    cart_item = current_end_user.cart_items.find_by(item_id: cart_item_params[:item_id])
+    if cart_item
+      cart_item.update(cart_item_params)
+      flash[:notice] = "Quantity was successfully updated"
       redirect_to public_end_user_cart_items_path
     else
-      @user = current_end_user
-      @carts= @user.cart_items
-      render "items/show(params[:item_id])"
+      if @cart.save
+        flash[:notice] = "Quantity was successfully created"
+        redirect_to public_end_user_cart_items_path
+      else
+        @item = Item.find(cart_item_params[:item_id])
+        @genres = Genre.all
+        @user = current_end_user
+        render template: "public/items/show"
+      end
     end
   end
   def update
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,11 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.where(status: "true")
     @genres = Genre.all
   end
   def show
     @item = Item.find(params[:id])
     @genres = Genre.all
+    @user = current_end_user
     @cart = CartItem.new
   end
 end
